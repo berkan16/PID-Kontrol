7192b16986249e6750a5f317c5a5e256cea01612
berkan16 <33376153+berkan16@users.noreply.github.com>
2020-05-13 02:30:03 +0300

    Update pidControlDriver.py

--- pidControlDriver.py
+++ pidControlDriver.py
@@ -1,19 +1,17 @@
-from pidControl import pidController
-import matplotlib.pyplot as plt
-
 def main():
    
-    # PID gain değerleri
+    # set PID value
     P = 2
     I = 0.001
     D = 0.5
     
     sampleTime = 30
-    desiredValue = 40
+    desiredValue = 50
     init_error = 0
     init_value = 0
-
-    system = lambda x : 0.1*(x**2)
+    
+    # set system
+    system = lambda x : 0.2*x + 0.5
     
     pid = pidController(P,I,D,init_error)
    
@@ -21,20 +19,24 @@ def main():
     pid.setSampleTime(sampleTime) 
     pid.setDesiredValue(desiredValue)
     
+    # define output
     output = [init_value]
     
+    # apply PID to the system
     for i in range(sampleTime):
         feedback = output[-1]
         pidResult = pid.pid(feedback)
         output.append(pidResult + feedback)
-    
+         
+    # plot input and output value
     plt.axhline(desiredValue, color = 'r', label = 'desired-value')
-    plt.plot(range(sampleTime+1), output, color = 'g', marker=".", label = 'output value')
+    plt.plot(range(sampleTime+1), output, color = 'b', marker=".", label = 'output value')
     plt.xlabel('time')
     plt.ylabel('output')
     plt.legend()
     plt.show()
     
+    # error reset
     pid.errorReset()
     
 main()
